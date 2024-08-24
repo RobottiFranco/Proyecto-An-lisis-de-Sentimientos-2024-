cf10812e08fedb1497bef39919f2b6d53cc00d80
RobottiFranco <franco.robotti0@gmail.com>
2024-08-24 10:46:29 -0300

    frases

---
+++ proyecto.py
@@ -0,0 +1,19 @@
+
+frases = [
+    "Excelente en su área, su muerte es una enorme pérdida y debería ser luto nacional",
+    "Estoy muy feliz con los resultados obtenidos en el examen",
+    "El clima está terrible hoy, no me gusta para nada",
+    "Es un gran avance para la ciencia",
+    "Estoy cansado y estresado por todo lo que sucede",
+    "Fue una sorpresa positiva ver que todo salió bien",
+    "Perder ese contrato fue una gran decepción",
+    "Estoy neutral respecto a la decisión tomada",
+    "Me encanta cómo se resolvió todo, fue excelente",
+    "Odio cuando las cosas no salen como espero",
+    "La película fue genial, pero el final fue un desastre",
+    "Me siento muy mal por lo que sucedió",
+    "El evento fue un éxito rotundo",
+    "Fue una gran pérdida para la compañía",
+    "Estoy completamente decepcionado con el servicio"
+]
+
